abfe086c2c41070d6f77dac28e099c34c8f24dee
Abdessamad404 <abdessamadzenasni@gmail.com>
2025-12-18 00:31:15 +0100

    create frontend views with Handlebars and styling

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require("express");
+import { helpers } from "./node_modules/handlebars/types/index.d";
+import { equals } from "./node_modules/sift/src/utils";
 const connectDB = require("./config/database");
 const app = express();
 const productRoutes = require("./routes/productRoutes");
-const { engine } = require('express-handlebars');
+const { engine } = require("express-handlebars");
 
-// Setup Handlebars
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+// Setup Handlebars template engine with custom helpers 
+app.engine(
+  "handlebars",
+  engine({
+    helpers: {
+      eq: (a, b) => a === b,
+    },
+  })
+);
 
-// Static files (CSS, images, etc.) 
-app.use(express.static('public'));
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+// Static files (CSS, images, etc.)
+app.use(express.static("public"));
 
 // Middleware to parse JSON (like Laravel automatically does)
 app.use(express.json());
