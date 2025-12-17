4f6ae0437b739edfd12eb41b130fc575978ecc22
Abdessamad404 <abdessamadzenasni@gmail.com>
2025-12-18 00:05:14 +0100

    setup Handlebars template engine

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const productRoutes = require("./routes/productRoutes");
+const { engine } = require('express-handlebars');
+
+// Setup Handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// Static files (CSS, images, etc.) 
+app.use(express.static('public'));
 
 // Middleware to parse JSON (like Laravel automatically does)
 app.use(express.json());
